d02a065c47f25b919e2bf031fec333e57f7b0b92
AnamikaBharti257 <anamika.22cse352@citranchi.ac.in>
2026-08-03 22:40:27 +0530

    java practice track p1

---
+++ track/Javapracticetrack/Simplerlearner.java
@@ -0,0 +1,16 @@
+package track.Javapracticetrack;
+
+public class Simplerlearner {
+
+    public static void main(String[] args) {
+        String learnername = "Ananya";
+        int completedproblems = 25;
+        double practiceHours = 2.5;
+        boolean projectcompleted = true;
+        System.out.println("learner: " + learnername);
+        System.out.println("problems completed: " + completedproblems);
+        System.out.println("practice hours: " + practiceHours);
+        System.out.println("project completed: " + projectcompleted);
+    }
+
+}
