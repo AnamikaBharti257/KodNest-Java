820d7dc9cef7d1c547560ab74e539cdfcd880e02
AnamikaBharti257 <anamika.22cse352@citranchi.ac.in>
2026-08-03 22:32:23 +0530

    calculate SI BMI marks percentage

---
+++ track/M02/T01/ST01/P01/Practice4.java
@@ -0,0 +1,19 @@
+package track.M02.T01.ST01.P01;
+
+public class Practice4 {
+
+    public static void main(String[] args) {
+
+        double simpleInterest = (10000.0 * 6.5 * 2.0) / 100.0;
+        double totalAmount = 10000.0 + simpleInterest;
+        double bmi = 72.0 / (1.8 * 1.8);
+        int totalmarks = 78 + 84 + 69 + 91 + 88;
+        double percentage = (totalmarks * 100.0) / 500;
+
+        System.out.println("Simple Interest: " + simpleInterest);
+        System.out.println("Total Amount: " + totalAmount);
+        System.out.println("BMI: " + bmi);
+        System.out.println("Total Marks: " + totalmarks);
+        System.out.println("Percentage: " + percentage);
+    }
+}
