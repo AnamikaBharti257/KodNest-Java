daf18a01686006ea68553de36b4f4cd76e63043d
AnamikaBharti257 <anamika.22cse352@citranchi.ac.in>
2026-08-04 12:26:53 +0530

    if else

---
+++ track/Day4/Practice2.java
@@ -0,0 +1,16 @@
+package track.Day4;
+
+public class Practice2 {
+
+    public static void main(String[] args) {
+        int num = 25;
+        if (num > 0) {
+            System.out.println("POSITIVE");
+
+        } else if (num < 0) {
+            System.out.println("NEGATIVE");
+        } else {
+            System.out.println("ZERO");
+        }
+    }
+}
