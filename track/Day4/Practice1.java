d07a7f51f3b50d64deb46307729eec408b7ccd59
AnamikaBharti257 <anamika.22cse352@citranchi.ac.in>
2026-08-04 11:34:42 +0530

    print and println

---
+++ track/Day4/Practice1.java
@@ -0,0 +1,14 @@
+package track.Day4;
+
+public class Practice1 {
+
+    public static void main(String[] args) {
+        System.out.println("Monday");
+        System.out.print("Tuesday");
+        System.out.println("wednesday");
+        System.out.println("Thursday");
+        System.out.print("Friday");
+        System.out.println("Saturday");
+        System.out.println("Sunday");
+    }
+}
