a718c59812df2ed8c79fcbb89f30382bbbf30c16
AnamikaBharti257 <anamika.22cse352@citranchi.ac.in>
2026-08-05 18:44:07 +0530

    if else to switch

---
+++ track/M02/T02/Switch.java
@@ -0,0 +1,38 @@
+package track.M02.T02;
+
+import java.util.Scanner;
+
+public class Switch {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int day = sc.nextInt();
+
+        switch (day) {
+            case 1:
+                System.out.println("Learning Day");
+                break;
+            case 2:
+                System.out.println("Learning Day");
+                break;
+            case 3:
+                System.out.println("Learning Day");
+                break;
+            case 4:
+                System.out.println("Learning Day");
+                break;
+            case 5:
+                System.out.println("Learning Day");
+                break;
+            case 6:
+                System.out.println("Revise Day");
+                break;
+            case 7:
+                System.out.println("Revise Day");
+                break;
+            default:
+                System.out.println("Invalid day");
+                break;
+        }
+    }
+}
