9e0a5b0c15b1e394b074977f124181e8b05d3ae3
AnamikaBharti257 <anamika.22cse352@citranchi.ac.in>
2026-08-05 19:40:25 +0530

    switch and ternary

---
+++ track/Javapracticetrack/practice3.java
@@ -0,0 +1,45 @@
+package track.Javapracticetrack;
+
+public class practice3 {
+
+    public static void main(String[] args) {
+
+        int roundCode = 4;
+        int attemptsUsed = 2;
+        int maxAttempts = 3;
+        String roundName;
+
+        switch (roundCode) {
+            case 1:
+                System.out.println("Current Round: Aptitude Round");
+                break;
+
+            case 2:
+                System.out.println("Current Round: Technical Round");
+                break;
+
+            case 3:
+                System.out.println("Current Round: HR Round");
+                break;
+
+            case 4:
+                System.out.println("Current Round: Final Review");
+                break;
+
+            case 5:
+                System.out.println("Current Round: Final Review");
+                break;
+
+            default:
+                System.out.println("Current Round: Invalid Round");
+                break;
+        }
+
+        String attemptStatus = (attemptsUsed < maxAttempts)
+                ? "Attempt Available"
+                : "Attempt Limit Reached";
+
+        System.out.println("Attempt Status: " + attemptStatus);
+    }
+
+}
