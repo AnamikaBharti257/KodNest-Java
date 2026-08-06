d956b6c27caced5f7475206da86527be6b51ae7e
AnamikaBharti257 <anamika.22cse352@citranchi.ac.in>
2026-08-06 12:29:43 +0530

    break nd continue

---
+++ track/M02/T02/Breakandcontinue.java
@@ -0,0 +1,17 @@
+package track.M02.T02;
+
+public class Breakandcontinue {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+
+            if (i == 6) {
+                continue;
+            }
+            System.out.println(i);
+            if (i == 9) {
+                break;
+            }
+        }
+    }
+}
