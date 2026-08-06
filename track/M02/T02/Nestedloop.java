d45e9e038ed274c5ab18fb8844b6ed823066aae6
AnamikaBharti257 <anamika.22cse352@citranchi.ac.in>
2026-08-06 11:43:53 +0530

    update

--- track/M02/T02/Nestedloop.java
+++ track/M02/T02/Nestedloop.java
@@ -3,24 +3,24 @@ package track.M02.T02;
 public class Nestedloop {
 
     public static void main(String[] args) {
-        //     for (int i = 0; i <= 5; i++) {
-        //         for (int j = 0; j <= 5; j++) {
-        //             System.out.println(j);
-        //         }
-        //         System.out.println();
-        //     }
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 0; j <= 5; j++) {
+                System.out.println(j);
+            }
+            System.out.println();
+        }
 
-        // //while nested
-        //  int i = 1;
-        //  while(i<=5){
-        //     int j = 1;
-        //     while(j<=5){
-        //         System.out.println(j);
-        //         j++;
-        //     }
-        //     System.out.println();
-        //     i++;
-        //  }
+        //while nested
+        int i = 1;
+        while (i <= 5) {
+            int j = 1;
+            while (j <= 5) {
+                System.out.println(j);
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
         //do while nested
         int i = 1;
         do {
