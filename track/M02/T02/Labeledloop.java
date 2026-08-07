aa9ae79e9a0514bd374cd90ff73cccb3a4bcee12
AnamikaBharti257 <anamika.22cse352@citranchi.ac.in>
2026-08-07 11:17:40 +0530

    taking user input

---
+++ track/M02/T02/Labeledloop.java
@@ -0,0 +1,17 @@
+package track.M02.T02;
+
+public class Labeledloop {
+
+    public static void main(String[] args) {
+        outer:
+        for (int i = 1; i <= 3; i++) {
+            inner:
+            for (int j = 1; j <= 4; j++) {
+                if (i == 2) {
+                    break outer;
+                }
+                System.out.println(i + " " + j);
+            }
+        }
+    }
+}
---
+++ track/M02/T03/Userinput.java
@@ -0,0 +1,38 @@
+package track.M02.T03;
+
+import java.util.Scanner;
+
+public class Userinput {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the byte value: ");
+        byte a = sc.nextByte();
+        System.out.println("byte value is: " + a);
+
+        System.out.println("Enter the short value: ");
+        short b = sc.nextShort();
+        System.out.println("short value is: " + b);
+
+        System.out.println("Enter the int value: ");
+        int c = sc.nextInt();
+        System.out.println("int value is: " + c);
+
+        System.out.println("Enter the long value: ");
+        long d = sc.nextLong();
+        System.out.println("long value is: " + d);
+
+        System.out.println("Enter the float value: ");
+        float e = sc.nextFloat();
+        System.out.println("float value is: " + e);
+
+        System.out.println("Enter the double value: ");
+        double f = sc.nextDouble();
+        System.out.println("double value is: " + f);
+
+        System.out.println("Enter the boolean value: ");
+        boolean h = sc.nextBoolean();
+        System.out.println("boolean value is: " + h);
+
+    }
+}
