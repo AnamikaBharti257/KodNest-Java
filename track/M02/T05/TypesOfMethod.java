ec3253923840a4510b2a975bfb53bb6405c71bc4
AnamikaBharti257 <anamika.22cse352@citranchi.ac.in>
2026-08-11 13:19:07 +0530

    student utility report

---
+++ track/M02/T05/TypesOfMethod.java
@@ -0,0 +1,23 @@
+package track.M02.T05;
+
+class pgm3 {
+
+    public static void main(String[] args) {
+
+        int res = add();
+        System.out.println(res);
+    }
+
+    static int add() {
+
+        int a = 10;
+        int b = 20;
+        int res = a + b;
+
+        return res;
+    }
+}
+
+public class TypesOfMethod {
+    
+}
