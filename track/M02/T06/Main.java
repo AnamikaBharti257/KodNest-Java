9b9336f16ddef8daee612334d0694ae7f00178db
AnamikaBharti257 <anamika.22cse352@citranchi.ac.in>
2026-08-14 13:10:08 +0530

    overload constructors

---
+++ track/M02/T06/Main.java
@@ -0,0 +1,43 @@
+package track.M02.T06;
+
+import java.util.Scanner;
+
+class FoodOrder {
+
+    String item;
+    int quantity;
+
+    FoodOrder(String item) {
+
+        this.item = item;
+        this.quantity = 1;
+    }
+
+    FoodOrder(String item, int quantity) {
+
+        this.item = item;
+        this.quantity = quantity;
+    }
+
+    void display() {
+        System.out.println("Order: " + item + ", Quantity: " + quantity);
+
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            String item = scanner.nextLine();
+            int quantity = scanner.nextInt();
+
+            FoodOrder f = new FoodOrder(item);
+            FoodOrder f2 = new FoodOrder(item, quantity);
+
+            f.display();
+            f2.display();
+        }
+    }
+
+}
