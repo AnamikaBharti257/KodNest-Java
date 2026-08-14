071d99cd5c74dfa8e5cc77a2d9375d3af86bc0b5
AnamikaBharti257 <anamika.22cse352@citranchi.ac.in>
2026-08-14 16:12:18 +0530

    code changed

--- track/MobileApp/Mobile.java
+++ track/MobileApp/Mobile.java
@@ -1,6 +1,18 @@
 package track.MobileApp;
 
-class Password {
+class TextPassword {
+}
+
+class Pattern {
+}
+
+class Face {
+}
+
+class Fingerprint {
+}
+
+class Voice {
 }
 
 class Access {
@@ -8,14 +20,31 @@ class Access {
 
 class AppLock {
 
-    void verify(Password p) {
-        System.out.println("Password verified");
+    void verifyText(TextPassword t) {
+        System.out.println("Text password verified");
+    }
+
+    void verifyPattern(Pattern p) {
+        System.out.println("Pattern verified");
+    }
+
+    void verifyFace(Face f) {
+        System.out.println("Face verified");
+    }
+
+    void verifyFingerprint(Fingerprint f) {
+        System.out.println("Fingerprint verified");
+    }
+
+    void verifyVoice(Voice v) {
+        System.out.println("Voice verified");
     }
 
     Access unlock() {
+
         Access a = new Access();
 
-        System.out.println("App unlocked");
+        System.out.println("Mobile unlocked");
 
         return a;
     }
@@ -27,9 +56,17 @@ public class Mobile {
 
         AppLock lock = new AppLock();
 
-        Password p = new Password();
+        TextPassword text = new TextPassword();
+        Pattern pattern = new Pattern();
+        Face face = new Face();
+        Fingerprint fingerprint = new Fingerprint();
+        Voice voice = new Voice();
 
-        lock.verify(p);
+        lock.verifyText(text);
+        lock.verifyPattern(pattern);
+        lock.verifyFace(face);
+        lock.verifyFingerprint(fingerprint);
+        lock.verifyVoice(voice);
 
         Access a = lock.unlock();
 
